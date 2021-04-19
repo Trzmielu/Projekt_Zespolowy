86c8d34eebeec0bda81349f62965dd0c2bdfe2fd
Trzmielu <eryk.podborny@gmail.com>
2021-04-19 19:41:11 +0200

    Queue mails

--- middleware/mailbuider.js
+++ middleware/mailbuider.js
@@ -4,7 +4,7 @@ async function MailSender(from, to, subject, text, deliverytime) {
   const DOMAIN = process.env.MG_DOMAIN;
   const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: DOMAIN});
   const data = {
-    from: "team@spambot.com",
+    from: from,
     to: to,
     subject: subject,
     text: text,
